9912415e2322af854decb51e0f0e74a763516be4
Julian Rojas <142496433+Sarikx@users.noreply.github.com>
2024-05-09 22:23:32 -0500

    Update cliente.h
    Añadir struct dentro de la libreria.

--- cliente.h
+++ cliente.h
@@ -1,19 +1,34 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
+
+struct Cliente {
+    long id;
+    char nombre[100];
+    char direccion[100];
+    long telefono;
+    int puntosVigentes;
+    int puntosRedimidos;
+    long fechaRedencion;
+    long fechaAsignados;
+};
 
 void mostrarMenu() {
     cout << "¡Bienvenido al programa!\n";
     cout << "========================\n";
-    cout << "1. Registrar Cliente\n";
-    cout << "2. Ingresar Puntos\n";
-    cout << "3. Consulta Puntos\n";
-    cout << "4. Consulta Clientes\n";
-    cout << "5. Redimir Puntos Textil\n";
-    cout << "6. Redimir Puntos electrodomesticos\n";
-    cout << "7. Redimir Puntos mercado\n";
+    cout << "1. Registrar cliente.\n";
+    cout << "2. Ingresar puntos.\n";
+    cout << "3. Consultar Puntos.\n";
+    cout << "4. Consultar clientes.\n";
+    cout << "5. Comprar textil usando puntos.\n";
+    cout << "6. Comprar electrodomesticos usando puntos.\n";
+    cout << "7. Comprar mercado usando puntos\n";
     cout << "8. Eliminar Clientes\n";
     cout << "9. Salir\n";
     cout << "========================\n";
     cout << "Ingrese el número de la opción deseada: ";
+}
+
+void registrarCliente() {
+    
 }
