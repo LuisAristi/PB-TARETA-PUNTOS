a20125f20886135f0ca6bc373d3764234ba61540
Julian Rojas <142496433+Sarikx@users.noreply.github.com>
2024-05-10 00:17:51 -0500

    Update cliente.h
    Avances

--- cliente.h
+++ cliente.h
@@ -6,19 +6,19 @@ using namespace std;
 struct Cliente {
     long id;
     char nombre[100];
-    char direccion[100];
+    char direccion[50];
     long telefono;
-    int puntosVigentes;
-    int puntosRedimidos;
+    float puntosVigentes = 0;
+    float puntosRedimidos = 0;
     long fechaRedencion;
-    long fechaAsignados;
+    long fechaAsignacion;
 };
 
 int mostrarMenu() {
     int opcion;
     
     system("cls");
-    cout << "¡Bienvenido al programa!\n";
+    cout << "SISTEMA DE PUNTOS DEL ALMACEN\n";
     cout << "========================\n";
     cout << "1. Registrar cliente.\n";
     cout << "2. Ingresar puntos.\n";
@@ -36,34 +36,82 @@ int mostrarMenu() {
     return opcion;
 }
 
-void registrarCliente() {
+void registrarCliente(struct Cliente cliente[], int n) {
+    for(int i=0; i<n; i++) {
+        cout << "\nIngrese la identificación del cliente #" << i+1 << ": "; cin >> cliente[i].id;
+        
+        // Verificar que la identificación no halla sido registrada previamente.
+        for(int j=0; j<n; j++) {
+            if(cliente[i].id == cliente[j].id && j != i) {
+                do {
+                    cout << "Identificación repetida, intentelo de nuevo: "; cin >> cliente[i].id;
+                }while(cliente[i].id == cliente[j].id);
+            }
+            break;
+        }
+
+        cout << "Nombre: "; fflush(stdin); gets(cliente[i].nombre);
+        cout << "Dirección: "; cin >> cliente[i].telefono;
+        cout << "Número de contacto: "; cin >> cliente[i].telefono;
+        //Función incompleta, faltan los datos: puntos vigentes, puntos redimidos, fecha ultima redención puntos, fecha últimos puntos asignados.
+    }
+}
+
+void ingresarPuntos(struct Cliente cliente[], int n) {
+    int id2;
+    cout << "Ingrese una identificación: "; cin >> id2;
+
+    // Se busca si la id ingresada esta registrada, si lo esta pide el dinero gastado y calcula en base a el la cantidad de puntos a almacenar.
+    for(int i=0; i<n; i++) {
+        if(id2 == cliente[i].id) {
+            int dinero;
+            cout << "Ingrese el monto del dinero gastado en compras: "; cin >> dinero;
+            // Por cada 100$ gastados se suma 1 punto vigente.
+            cliente[i].puntosVigentes += dinero/100;
+
+        }else {
+            cout << "\nIdentificación no encontrada...\n";
+            break;
+        }
+
+        cout << "\nSus puntos actuales son: " << cliente[i].puntosVigentes;
+    }
+}
+
+void consultarPuntos(struct Cliente cliente[], int n) {
+    int id2;
+    cout << "Ingrese una identificación: "; cin >> id2;
+
+    // Se busca si la id ingresada esta registrada, si lo esta muestra el nombre del cliente, la cantidad de puntos vigentes, puntos redimidos, fecha de la última asignación de puntos, fecha de la última redención de puntos.
+    for(int i=0; i<n; i++) {
+        if(id2 == cliente[i].id) {
+            cout << "\nNombre: " << cliente[i].nombre;
+            cout << "Puntos vigentes: " << cliente[i].puntosVigentes;
+            cout << "Puntos redimidos: " << cliente[i].puntosRedimidos;
+            cout << "Fecha de la última asignación de puntos: " << cliente[i].fechaAsignacion;
+            cout << "Fecha de la última redención de puntos: " << cliente[i].fechaRedencion;
+        }else {
+            cout << "\nIdentificación no encontrada...\n";
+            break;
+        }
+}
+
+void listarClientes(struct Cliente cliente[], int n) {
 
 }
 
-void ingresarPuntos() {
+void comprarTextil(struct Cliente cliente[], int n) {
 
 }
 
-void consultarPuntos() {
+void comprarElectrodomestico(struct Cliente cliente[], int n) {
 
 }
 
-void consultarClientes() {
+void comprarMercado(struct Cliente cliente[], int n) {
 
 }
 
-void comprarTextil() {
-
-}
-
-void comprarElectrodomestico() {
-
-}
-
-void comprarMercado() {
-
-}
-
-void eliminarClientes() {
+void eliminarClientes(struct Cliente cliente[], int n) {
 
 }
