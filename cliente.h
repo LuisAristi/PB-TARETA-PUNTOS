873735d41de5b57881543ec381707e0f360f286d
LuisAristi <santiaristi2015@gmail.com>
2024-05-11 00:08:35 -0500

    se sufre pero se goza

--- cliente.h
+++ cliente.h
@@ -1,167 +1,367 @@
-#include <iostream>
-#include <unistd.h>
-#include <stdio.h>
 #include <ctime> //esta libreria permite no tener que pedir el tiempo, lo ingresara de una al sistema
 #include <iomanip>
+#include <iostream>
 #include <locale>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 //#include <windows.h>
 
 using namespace std;
 
 typedef struct Cliente {
-    long id;
-    char nombre[100];
-    char direccion[50];
-    long telefono;
-    float puntosVigentes = 0;
-    float puntosRedimidos = 0;
-    std::tm fechaRedencion; //un struct que guardara fecha, muy util
-    std::tm fechaAsignacion; //este otro tambien, por lo que nos permitira tener mas orden
-}cliente;
-
-/* Documentacion de ctime en codigo
-
-std::tm fecha;
-fecha.tm_year = 123; | 2023 - 1900 = 123
-fecha.tm_mon = 4;    | Mayo es el quinto mes, pero se representa como 4
-fecha.tm_mday = 10;
-*/
-
-/* cosas utiles
-
-// Obtener la fecha y hora actual
-    std::time_t now = std::time(nullptr);
-    
-    // Convertir la fecha y hora actual a una estructura tm
-    std::tm* localTime = std::localtime(&now);
-    
-    // Extraer el día, mes y año
-    int dia = localTime->tm_mday;
-    int mes = localTime->tm_mon + 1; // Sumamos 1 porque los meses van de 0 a 11
-    int anio = localTime->tm_year + 1900; // Sumamos 1900 porque tm_year representa años desde 1900
-
-    // Mostrar la fecha actual
-    std::cout << "Fecha actual: " << dia << "/" << mes << "/" << anio << std::endl;
-*/
-
+  long int id = 0;
+  char nombre[50];
+  char direccion[50];
+  long long telefono = 0;
+  int puntosVigentes = 0;
+  int puntosRedimidos = 0;
+  std::tm fechaRedencion;  // un struct que guardara fecha, muy util
+  std::tm fechaAsignacion; // este otro tambien, por lo que nos permitira tener
+                           // mas orden
+} cliente;
 
 int mostrarMenu() {
-    int opcion;
+  int opcion;
+
+  system("cls");
+  std::cout << "SISTEMA DE PUNTOS DEL ALMACEN\n";
+  std::cout << "========================\n";
+  std::cout << "1. Registrar cliente.\n";
+  std::cout << "2. Ingresar puntos.\n";
+  std::cout << "3. Consultar Puntos.\n";
+  std::cout << "4. Consultar clientes.\n";
+  std::cout << "5. Comprar textil usando puntos.\n";
+  std::cout << "6. Comprar electrodomesticos usando puntos.\n";
+  std::cout << "7. Comprar mercado usando puntos.\n";
+  std::cout << "8. Eliminar Clientes.\n";
+  std::cout << "9. Salir.\n";
+  std::cout << "========================\n";
+  std::cout << "Ingrese el número de la opción deseada: ";
+  cin >> opcion;
+  system("cls");
+  return opcion;
+}
+
+void horaAsignacion(Cliente cliente[], int x){
+    std::time_t tiempoActual = std::time(NULL); // Obtener el tiempo actual en segundos
+    std::tm* fechaHora = std::localtime(&tiempoActual); // Convertir el tiempo a una estructura tm
     
-    system("cls");
-    cout << "SISTEMA DE PUNTOS DEL ALMACEN\n";
-    cout << "========================\n";
-    cout << "1. Registrar cliente.\n";
-    cout << "2. Ingresar puntos.\n";
-    cout << "3. Consultar Puntos.\n";
-    cout << "4. Consultar clientes.\n";
-    cout << "5. Comprar textil usando puntos.\n";
-    cout << "6. Comprar electrodomesticos usando puntos.\n";
-    cout << "7. Comprar mercado usando puntos.\n";
-    cout << "8. Eliminar Clientes.\n";
-    cout << "9. Salir.\n";
-    cout << "========================\n";
-    cout << "Ingrese el número de la opción deseada: ";
-    cin >> opcion;
-    system("cls");
-    return opcion;
+    // Crear un struct Fecha y asignar los componentes de la fecha actual
+    cliente[x].fechaAsignacion.tm_mday = (*fechaHora).tm_mday;
+    cliente[x].fechaAsignacion.tm_mon = (*fechaHora).tm_mon; // Sumamos 1 porque los meses van de 0 a 11
+    cliente[x].fechaAsignacion.tm_year = (*fechaHora).tm_year; // Sumamos 1900 porque tm_year representa años desde 1900
+}
+
+void horaRedencion(Cliente cliente[], int x){
+    std::time_t tiempoActual = std::time(NULL); // Obtener el tiempo actual en segundos
+    std::tm* fechaHora = std::localtime(&tiempoActual); // Convertir el tiempo a una estructura tm
+    
+    // Crear un struct Fecha y asignar los componentes de la fecha actual
+    cliente[x].fechaRedencion.tm_mday = (*fechaHora).tm_mday;
+    cliente[x].fechaRedencion.tm_mon = (*fechaHora).tm_mon; // Sumamos 1 porque los meses van de 0 a 11
+    cliente[x].fechaRedencion.tm_year = (*fechaHora).tm_year; // Sumamos 1900 porque tm_year representa años desde 1900
+}
+
+// Busca la id ingresada en el registro de clientes, si lo encuentra retorna
+// verdadero.
+bool buscarId(long int id, Cliente cliente[], int& n, int x) {
+  for (int i = 0; i < x; i++) {
+    if (id == cliente[i].id) {
+      n = i;
+      return true;
+    }
+  }
+  return false;
 }
 
 void registrarCliente(Cliente cliente[], int n) {
-    int aux;
-    for(int i=0; i<n; i++) {
-        cout << "\nIngrese la identificación del cliente #" << i+1 << ": "; cin >> cliente[i].id;
-        
-        // Verificar que la identificación no halla sido registrada previamente.
-        //seria mejor que una funcion se encargara de esto en tecnico.h --luis Aristi
-        //y retornara un booleano aqui para verificar --luis aristi
-        for(int j=0; j<n; j++) {
-            if(cliente[i].id == cliente[j].id && j != i) {
-                do {
-                    cout << "Identificación repetida, intentelo de nuevo: "; cin >> cliente[i].id;
-                }while(cliente[i].id == cliente[j].id);
-            }
-            break;
-        }
+  int aux;
+  int posicion;
+  for (int i = 0; i < n; i++) {
+    std::cout << "\nIngrese la identificación del cliente #" << i + 1 << ": ";
+    cin >> cliente[i].id;
 
-		std::cout<<"Nombre: "; fflush(stdin); fgets(cliente[i].nombre, 50, stdin);
-		std::cout<<"Direccion: "; fflush(stdin); fgets(cliente[i].direccion, 20, stdin);
-		std::cout<<"Telefono: "; fflush(stdin); std::cin>>cliente[i].telefono;
-        std::cout<<"Puntos Vigentes: "; fflush(stdin); std::cin>>cliente[i].puntosVigentes;
-        std::cout<<"Puntos Redimidos: "; fflush(stdin); std::cin>>cliente[i].puntosRedimidos;
-        std::cout<<"Ingrese Dia Ultima Redencion de Puntos: "; fflush(stdin); std::cin>>cliente[i].fechaAsignacion.tm_mday;
-        std::cout<<"Ingrese Mes Ultima Redencion de Puntos: "; fflush(stdin); std::cin>>cliente[i].fechaAsignacion.tm_mon;
-        std::cout<<"Ingrese Fecha Anual Ultima Redencion de Puntos: "; fflush(stdin); std::cin>>aux;
-        aux = aux - 1900;
-        cliente[i].fechaAsignacion.tm_mon = aux;
-        system("cls");
+    // Verificar si la identificación ha sido registrada previamente, para
+    // evitar que se repita.
+    do {
+      if (buscarId(cliente[i].id, cliente, n, i)) {
+        std::cout << "Identificación repetida, intentelo de nuevo: ";
+        cin >> cliente[i].id;
+      }
+    } while (buscarId(cliente[i].id, cliente, n, i));
 
-    }
+    std::cout << "Nombre: ";
+    fflush(stdin);
+    fgets(cliente[i].nombre, 50, stdin);
+    std::cout << "Direccion: ";
+    fflush(stdin);
+    fgets(cliente[i].direccion, 50, stdin);
+    std::cout << "Telefono: ";
+    fflush(stdin);
+    cin >> cliente[i].telefono;
+    std::cout << "Puntos Vigentes: ";
+    fflush(stdin);
+    cin >> cliente[i].puntosVigentes;
+    std::cout << "Puntos redimidos: ";
+    fflush(stdin);
+    cin >> cliente[i].puntosRedimidos;
+    std::cout << "Ingrese el dia de la última Asignacion de puntos: ";
+    fflush(stdin);
+    cin >> cliente[i].fechaAsignacion.tm_mday;
+    std::cout << "Ingrese el mes de la última Asignacion de puntos: ";
+    fflush(stdin);
+    cin >> aux;
+    cliente[i].fechaAsignacion.tm_mon = aux - 1;
+    std::cout << "Ingrese el año de la última Asignacion de puntos: ";
+    fflush(stdin);
+    cin >> aux;
+    cliente[i].fechaAsignacion.tm_year = aux - 1900;
+
+    std::cout << "Ingrese el dia de la última Redencioncion de puntos: ";
+    fflush(stdin);
+    cin >> cliente[i].fechaRedencion.tm_mday;
+    std::cout << "Ingrese el mes de la última Redencioncion de puntos: ";
+    fflush(stdin);
+    cin >> aux;
+    cliente[i].fechaRedencion.tm_mon = aux - 1;
+    std::cout << "Ingrese el año de la última Redencion de puntos: ";
+    fflush(stdin);
+    cin >> aux;
+    cliente[i].fechaRedencion.tm_year = aux - 1900;
+
+    system("cls");
+  }
 }
 
 void ingresarPuntos(struct Cliente cliente[], int n) {
-    int id2;
-    cout << "Ingrese una identificación: "; cin >> id2;
+  long int id;
+  std::cout << "Ingrese una identificación: ";
+  cin >> id;
 
-    // Se busca si la id ingresada esta registrada, si lo esta pide el dinero gastado y calcula en base a el la cantidad de puntos a almacenar.
-    for(int i=0; i<n; i++) {
-        if(id2 == cliente[i].id) {
-            int dinero;
-            cout << "Ingrese el monto de la compra: "; cin >> dinero;
-            // Por cada 100$ gastados se suma 1 punto vigente.
-            cliente[i].puntosVigentes += dinero/100;
+  // Se busca si la id ingresada esta registrada, si lo esta pide el dinero
+  // gastado y calcula en base a el la cantidad de puntos a almacenar.
+  int aux; // Entra por referencia a la función para guardar la posicion del
+         // cliente en el arreglo.
+  if (buscarId(id, cliente, aux, n)){
+    int dinero = 0;
+    std::cout << "Ingrese la cantidad de dinero gastado: ";
+    cin >> dinero;
+    // Por cada 100$ gastados se suma 1 punto vigente.
+    cliente[aux].puntosVigentes += (dinero / 100);
+    std::cout << "\nSus puntos actuales son: " << cliente[aux].puntosVigentes;
+    horaAsignacion(cliente, aux);
 
-        }else {
-            cout << "\nIdentificación no encontrada...\n";
-            break;
-        }
-
-        cout << "\nSus puntos actuales son: " << cliente[i].puntosVigentes;
-    }
+  } else {
+    std::cout << "\nIdentificación no encontrada...\n";
+  }
 }
 
 void consultarPuntos(struct Cliente cliente[], int n) {
-    int id2;
-    cout << "Ingrese una identificación: "; cin >> id2;
+  int id;
+  std::cout << "Ingrese una identificación: ";
+  cin >> id;
 
-    // Se busca si la id ingresada esta registrada, si lo esta muestra el nombre del cliente, la cantidad de puntos vigentes, puntos redimidos, fecha de la última asignación de puntos, fecha de la última redención de puntos.
-    for(int i=0; i<n; i++) {
-        if(id2 == cliente[i].id) {
-            cout << "\nNombre: " << cliente[i].nombre;
-            cout << "Puntos vigentes: " << cliente[i].puntosVigentes;
-            cout << "Puntos redimidos: " << cliente[i].puntosRedimidos;
+  // Se busca si la id ingresada esta registrada, si lo esta muestra el nombre
+  // del cliente, la cantidad de puntos vigentes, puntos redimidos, fecha de la
+  // última asignación de puntos, fecha de la última redención de puntos.
+  int x;
+  if (buscarId(id, cliente, x, n)) {
+    std::cout << "\nNombre: " << cliente[x].nombre<<std::endl;
+    std::cout << "Puntos vigentes: " << cliente[x].puntosVigentes<<std::endl;
+    std::cout << "Puntos redimidos: " << cliente[x].puntosRedimidos<<std::endl;
 
-            //deberia crear una funcion para esto pero quiero corregir los errores de compilacion --luis Aristi
-            int dia = cliente[i].fechaAsignacion.tm_mday;
-            int mes = cliente[i].fechaAsignacion.tm_mon + 1; // Sumamos 1 porque los meses van de 0 a 11
-            int anio = cliente[i].fechaAsignacion.tm_year + 1900; // Sumamos 1900 porque tm_year representa años desde 1900
+    // deberia crear una funcion para esto pero quiero corregir los errores de
+    // compilacion --luis Aristi
+    int dia = cliente[x].fechaAsignacion.tm_mday;
+    int mes = cliente[x].fechaAsignacion.tm_mon +
+              1; // Sumamos 1 porque los meses van de 0 a 11
+    int anio = cliente[x].fechaAsignacion.tm_year +
+               1900; // Sumamos 1900 porque tm_year representa años desde 1900
 
+    int diaR = cliente[x].fechaRedencion.tm_mday;
+    int mesR = cliente[x].fechaRedencion.tm_mon +
+              1; // Sumamos 1 porque los meses van de 0 a 11
+    int anioR = cliente[x].fechaRedencion.tm_year +
+               1900; // Sumamos 1900 porque tm_year representa años desde 1900
 
-            cout << "Fecha de la última asignación de puntos: " <<dia<<"/"<<mes<<"/"<<anio;
-            cout << "Fecha de la última redención de puntos: "; //cliente[i].fechaRedencion; sera corregido lo prometo --luis Aristi
-        }else {
-            cout << "\nIdentificación no encontrada...\n";
-            break;
-        }
-    }
+    std::cout << "Fecha de la última asignación de puntos: " << dia << "/"
+              << mes << "/" << anio<<std::endl;
+    std::cout
+        << "Fecha de la última redención de puntos: " << diaR << "/"
+              <<mesR<< "/" << anioR<<std::endl; 
+  } else {
+    std::cout << "\nIdentificación no encontrada...\n";
+  }
 }
 
-void listarClientes(struct Cliente cliente[], int n){
-    cout<<"hola";
+void listarClientes(struct Cliente cliente[], int n) {
+  // Algoritmo de ordenamiento de burbuja
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (strcmp(cliente[j].nombre, cliente[j + 1].nombre) > 0) {
+        // Intercambiar nombres[j] y nombres[j + 1]
+        char temp[50];
+        strcpy(temp, cliente[j].nombre);
+        strcpy(cliente[j].nombre, cliente[j + 1].nombre);
+        strcpy(cliente[j + 1].nombre, temp);
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    std::cout << "Identificación: " << cliente[i].id << endl
+              << "Nombre: " << cliente[i].nombre << endl
+              << "Puntos vigentes: " << cliente[i].puntosVigentes << endl;
+  }
 }
 
 void comprarTextil(struct Cliente cliente[], int n) {
-    cout<<"hola";
+  int id;
+  std::cout << "Ingrese una identificación: ";
+  cin >> id;
+
+  // Se busca si la id ingresada esta registrada, si lo esta, se pide el valor
+  // de la compra.
+  int x; // Entra por referencia a la función para guardar la posicion del
+         // cliente en el arreglo.
+  if (buscarId(id, cliente, x, n)) {
+    float precio;
+    std::cout << "Ingrese el precio de la compra: ";
+    cin >> precio;
+
+    // verifica si tiene suficientes puntos.
+    if (cliente[x].puntosVigentes < 5000) {
+      std::cout << "No tiene suficientes puntos para redimir.";
+      return;
+    }
+
+    // Se calcula el descuento. (el descuento es del 40% si el valor de la
+    // compra es mayor a $100.000)
+    if (precio > 100000) {
+      int descuento = precio * 0.4;
+      precio -= descuento;
+      cliente[x].puntosVigentes -= 5000;
+      cliente[x].puntosRedimidos += 5000;
+      std::cout << "Has redimido 5000 puntos. Tu nuevo total de compra es: $"
+                << precio << endl;
+      horaRedencion(cliente, x);
+    } else {
+      std::cout
+          << "El valor de la compra debe ser mayor a $100.000 para redimir "
+             "puntos."
+          << endl;
+    }
+  }
 }
 
 void comprarElectrodomestico(struct Cliente cliente[], int n) {
-    cout<<"hola";
+  int id;
+  std::cout << "Ingrese una identificación: ";
+  cin >> id;
+
+  // Se busca si la id ingresada esta registrada, si lo esta, se pide el valor
+  // de la compra.
+  int x;
+  if (buscarId(id, cliente, x, n)) {
+    float precio;
+    std::cout << "Ingrese el precio de la compra: ";
+    cin >> precio;
+
+    // verifica si tiene suficientes puntos.
+    if (cliente[x].puntosVigentes < 8000) {
+      std::cout << "No tiene suficientes puntos para redimir.";
+      return;
+    }
+
+    // Se calcula el descuento. (el descuento es del 25% si el valor de la
+    // compra es mayor a $500.000)
+    if (precio > 500000) {
+      int descuento = precio * 0.25;
+      precio -= descuento;
+      cliente[x].puntosVigentes -= 8000;
+      cliente[x].puntosRedimidos += 8000;
+      std::cout << "Has redimido 8000 puntos. Tu nuevo total de compra es: $"
+                << precio << endl;
+      horaRedencion(cliente, x);
+    } else {
+      std::cout
+          << "El valor de la compra debe ser mayor a $500.000 para redimir "
+             "puntos."
+          << endl;
+    }
+  }
 }
 
 void comprarMercado(struct Cliente cliente[], int n) {
-    cout<<"hola";
+  int id;
+  std::cout << "Ingrese una identificación: ";
+  cin >> id;
+
+  // Se busca si la id ingresada esta registrada, si lo esta, se pide el valor
+  // de la compra.
+  int x;
+  if (buscarId(id, cliente, x, n)) {
+    float precio;
+    std::cout << "Ingrese el precio de la compra: ";
+    cin >> precio;
+
+    // verifica si tiene suficientes puntos.
+    if (cliente[x].puntosVigentes < 8000) {
+      std::cout << "No tiene suficientes puntos para redimir.";
+      return;
+    }
+
+    // Se calcula el descuento. (el descuento es del 10% si el valor de la
+    // compra es mayor a $100.000)
+    if (precio > 100000) {
+      int descuento = precio * 0.1;
+      precio -= descuento;
+      cliente[x].puntosVigentes -= 10000;
+      cliente[x].puntosRedimidos += 10000;
+      std::cout << "Has redimido 8000 puntos. Tu nuevo total de compra es: $"
+                << precio << endl;
+      horaRedencion(cliente, x);
+    } else {
+      std::cout
+          << "El valor de la compra debe ser mayor a $100.000 para redimir "
+             "puntos."
+          << endl;
+    }
+  }
 }
 
 void eliminarClientes(struct Cliente cliente[], int n) {
-    cout<<"hola";
+  long cod_2;
+  std::cout << "Digite la identificacion del cliente que quiere eliminar: "
+            << endl;
+  cin >> cod_2;
+
+  int a = 0;
+
+  for (int x = 0; x < n; x++) {
+    if (cliente[x].id == cod_2) {
+      std::cout << "Se ha eliminado al cliente " << cliente[x].nombre << endl;
+      cliente[x].id = 0;
+      cliente[x].telefono = 0;
+      cliente[x].puntosVigentes = 0;
+      cliente[x].puntosRedimidos = 0;
+      cliente[x].fechaRedencion.tm_mday = 0;
+      cliente[x].fechaRedencion.tm_mon = 0;
+      cliente[x].fechaRedencion.tm_year = 0;
+      cliente[x].fechaAsignacion.tm_mday = 0;
+      cliente[x].fechaAsignacion.tm_mon = 0;
+      cliente[x].fechaAsignacion.tm_year = 0;
+      for (int i = 0; i < 50; i++) {
+        cliente[x].nombre[i] = '\0';
+        cliente[x].direccion[i] = '\0';
+      }
+      a = 1;
+      break;
+    }
+  }
+
+  if (a == 0) {
+    std::cout << "No se ha encontrado al cliente, intentelo denuevo" << endl;
+  }
 }
--- main.c++
+++ main.c++
@@ -1,58 +1,111 @@
 #include "cliente.h"
+#include <locale>
 
-using namespace std; 
+using namespace std;
 
-int main(){
-    setlocale(LC_ALL, "spanish");
+int main() {
+  setlocale(LC_ALL, "spanish");
 
-    //Ventana de presentación.
-    cout << "BIENVENIDO A NUESTRO SOFTWARE, ESTE PROGRAMA HA SIDO DESARROLLADO POR:" << endl; sleep(1000);
-    cout << "\nJulian Rojas: 0222320040        Eduardo Tuñon: 0222320052        Luis Aristizaval: \nEstudiantes de Ingenieria de sistemas.\n\n" << endl;
+  // Ventana de presentación.
+  cout << "BIENVENIDO A NUESTRO SOFTWARE, ESTE PROGRAMA HA SIDO DESARROLLADO "
+          "POR:"
+       << endl;
+  //Sleep(1000);
+  cout << "\nJulian Rojas: 0222320040        Eduardo Tuñon: 0222320052        "
+          "Luis Aristizabal: 0222310005\nEstudiantes de Ingenieria de "
+          "sistemas.\n\n"
+       << endl;
 
-    cout << "Este programa imita el manejo de un sistema de puntos acumulables por cada compra en un almacen.\n" << endl;
-    cout << "Con opciónes para: \n";
+  cout << "Este programa imita el manejo de un sistema de puntos acumulables "
+          "por cada compra en un almacen.\n"
+       << endl;
+  cout << "Con opciónes para: \n";
 
-    cout << "\n    1. Registrar un cliente: Almacenando los datos de documento de identificación (id), nombre,\n    dirección, teléfono, puntos vigentes, puntos redimidos, fecha ultima redención puntos,\n    fecha últimos puntos asignados.\n";
+  cout << "\n    1. Registrar un cliente: Almacenando los datos de documento "
+          "de identificación (id), nombre,\n    dirección, teléfono, puntos "
+          "vigentes, puntos redimidos, fecha ultima redención puntos,\n    "
+          "fecha últimos puntos asignados.\n";
 
-    cout << "\n    2. Ingresar puntos: Se pide el id del usuario, luego se busca y se agregan los puntos actuales,\n    que se calculan tal que; por cada 100$ gastados se le sumaran al comprador 1 punto, es decir\n    que si gasta 5500$ se le sumaran 55 puntos.\n";
+  cout << "\n    2. Ingresar puntos: Se pide el id del usuario, luego se busca "
+          "y se agregan los puntos actuales,\n    que se calculan tal que; por "
+          "cada 100$ gastados se le sumaran al comprador 1 punto, es decir\n   "
+          " que si gasta 5500$ se le sumaran 55 puntos.\n";
 
-    cout << "\n    3. Consultar, con dos funciones: \n";
-    cout << "        a. Consultar puntos: Al ingresar el id del cliente se visualizará el nombre del cliente,\n        la cantidad de puntos vigentes, puntos redimidos, fecha de la última asignación de puntos,\n        fecha de la última redención de puntos.\n";
-    cout << "        b. Listar Clientes: Genera un listado de todos los clientes con su respectivo\n        documento de identificación (id), nombre y sus puntos vigentes.\n";
+  cout << "\n    3. Consultar, con dos funciones: \n";
+  cout << "        a. Consultar puntos: Al ingresar el id del cliente se "
+          "visualizará el nombre del cliente,\n        la cantidad de puntos "
+          "vigentes, puntos redimidos, fecha de la última asignación de "
+          "puntos,\n        fecha de la última redención de puntos.\n";
+  cout << "        b. Listar Clientes: Genera un listado de todos los clientes "
+          "con su respectivo\n        documento de identificación (id), nombre "
+          "y sus puntos vigentes (Ordenado alfabeticamente por el nombre).\n";
 
-    cout << "\n    4. Redimir Puntos:";
-    cout << "\n    En esta opción se debe ingresar el documento de identificación, buscar cliente y verificar\n    si tiene suficientes puntos para poder redimir, en caso afirmativo descontar los puntos redimidos\n    y actualizar en el registro del cliente.\n";
-    cout << "\n        a. Compra Textil: será equivalente a un descuento del 40% del valor de la compra\n        si esta es mayor a $100.000. Para este caso se redimirán 5.000 puntos.\n";
-    cout << "\n        b. Compra Electrodomésticos: será equivalente a un descuento del 25% del valor de la compra,\n        si esta es mayor a $500.000. Para este caso se redimirán 8.000 puntos\n";
-    cout << "\n        c. Compra Mercado: será equivalente a un descuento del 10% del valor de la compra\n        si está es mayor a $100.000. Para este caso se redimirán 10.000 puntos\n";
+  cout << "\n    4. Redimir Puntos:";
+  cout
+      << "\n    En esta opción se debe ingresar el documento de "
+         "identificación, buscar cliente y verificar\n    si tiene suficientes "
+         "puntos para poder redimir, en caso afirmativo descontar los puntos "
+         "redimidos\n    y actualizar en el registro del cliente.\n";
+  cout << "\n        a. Compra Textil: será equivalente a un descuento del 40% "
+          "del valor de la compra\n        si esta es mayor a $100.000. Para "
+          "este caso se redimirán 5.000 puntos.\n";
+  cout << "\n        b. Compra Electrodomésticos: será equivalente a un "
+          "descuento del 25% del valor de la compra,\n        si esta es mayor "
+          "a $500.000. Para este caso se redimirán 8.000 puntos\n";
+  cout << "\n        c. Compra Mercado: será equivalente a un descuento del "
+          "10% del valor de la compra\n        si está es mayor a $100.000. "
+          "Para este caso se redimirán 10.000 puntos\n";
 
-    cout << "\n    5. Eliminar Clientes: Al ingresar el documento de identificación de un cliente se debe\n    eliminar el registro correspondiente a ese cliente.\n\n";
+  cout << "\n    5. Eliminar Clientes: Al ingresar el documento de "
+          "identificación de un cliente se debe\n    eliminar el registro "
+          "correspondiente a ese cliente.\n\n";
 
+  system("pause");
+  system("cls");
+
+  int n;
+  cout << "Ingrese la cantidad de clientes: ";
+  cin >> n;
+
+  Cliente cliente[n];
+
+  int opcion;
+
+  do {
+    opcion = mostrarMenu();
+
+    switch (opcion) {
+    case 1:
+      registrarCliente(cliente, n);
+      break;
+    case 2:
+      ingresarPuntos(cliente, n);
+      break;
+    case 3:
+      consultarPuntos(cliente, n);
+      break;
+    case 4:
+      listarClientes(cliente, n);
+      break;
+    case 5:
+      comprarTextil(cliente, n);
+      break;
+    case 6:
+      comprarElectrodomestico(cliente, n);
+      break;
+    case 7:
+      comprarMercado(cliente, n);
+      break;
+    case 8:
+      eliminarClientes(cliente, n);
+      break;
+    case 9:
+      cout << "\nSaliendo...\n\n";
+      break;
+    default:
+      cout << "\nOpción invalida...\n\n";
+      break;
+    }
     system("pause");
-    system("cls");
-    
-    int n;
-    cout << "Ingrese la cantidad de clientes: "; cin >> n;
-    
-    Cliente cliente[n];
-    
-    int opcion;
-
-    do {
-        opcion = mostrarMenu();
-        
-        switch (opcion) {
-            case 1: registrarCliente(cliente, n); break;
-            case 2: ingresarPuntos(cliente, n); break;
-            case 3: consultarPuntos(cliente, n); break;     
-            case 4: //listarClientes(cliente, n); break;
-            case 5: //comprarTextil(cliente, n); break;
-            case 6: //comprarElectrodomestico(cliente, n); break;
-            case 7: //comprarMercado(cliente, n); break;
-            case 8: //eliminarClientes(cliente, n); break;
-            case 9: cout << "\nSaliendo...\n\n"; break;
-            default: cout << "\nOpción invalida...\n\n"; break;
-        }
-        system("pause");
-    } while (opcion != 9);
+  } while (opcion != 9);
 }
